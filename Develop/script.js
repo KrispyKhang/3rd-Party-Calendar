9763f4795986c6185950102c96c696f528aae4c5
Khang Tran <creamybanana@Khang.local>
2023-10-05 17:54:11 -0700

    added html divs and restructured code

--- Develop/script.js
+++ Develop/script.js
@@ -8,7 +8,7 @@ dayjs.locale(localeSettings);
 // wait until the DOM is fully loaded before executing code inside the function below
 $(function () {
   //  gets the current hour of the day using DayJS
-  const currentHour = dayjs(). format('H');
+  const currentHour = dayjs().format('H');
 
 
   // -- FUNCTION 2 --
@@ -19,76 +19,85 @@ $(function () {
       // inside the .each loop, the line extracts the class of the current block.
       // 'this' refers to the current DOM element being processed and converts it into
       // an integer using parseINT
-      const blockHour = parseInt(this.id); 
-
+      const blockHour = parseInt(this.id);
       // 'this' is being represented by each element with the class time-block
 
       // checks to see if 'blockHour' - block represented by the current time
       // is less than 'currentHour' (current hour of the day). if this condition
       // is true, it adds a CSS class 'past'  to the current time block element. 
       // if condition is false, removes the class 'past'
-      
+
       // this goes applies for 'past', 'present', 'future'
-      $(this).toggleclass('past', blockHour < currentHour);
-      $(this).toggleclass('present', blockHour === currentHour);
-      $(this).toggleclass('future', blockHour > currentHour);      
+      $(this).toggleClass('past', blockHour < currentHour);
+      $(this).toggleClass('present', blockHour === currentHour);
+      $(this).toggleClass('future', blockHour > currentHour);
     });
   };
 
-// -- FUNCTION 3 --
-// function below saves user's input into localStorage
-function textEntry() {
-  // uses Jquery elements to target all elements with the class of '.saveBtn' and attaches
-  // a click event handler to them -  whenever the user clicks - the click event handler will be executed
-  $('.saveBtn').on('click', function(){
+  // -- FUNCTION 3 --
+  // function below saves user's input into localStorage
+  function textEntry() {
+    // uses Jquery elements to target all elements with the class of '.saveBtn' and attaches
+    // a click event handler to them -  whenever the user clicks - the click event handler will be executed
+    $('.saveBtn').on('click', function() {
 
-    // inside click event handler, the liner retrieves the ID of the parent element of .savBTN
-    const key = $(this).parent().attr('id');
-    // retrieved the .description class sibling when user clicks on saveBtn and retrieves
-    // its value whenever the user inputs text in the <textarea>
-    const value = $(this).siblings('.description').val();
+      // inside click event handler, the liner retrieves the ID of the parent element of .savBTN
+      const key = $(this).parent().attr('id');
+      // retrieved the .description class sibling when user clicks on saveBtn and retrieves
+      // its value whenever the user inputs text in the <textarea>
+      const value = $(this).siblings('.description').val();
+      localStorage.setItem(key, value);
+    });
+  }
 
-    localStorage.setItem(key, value);
-  });
-}
-
-// -- FUNCTION 4 --
-// function to change color block based on past (grey, present (red), future (green), relative to the currentHour
-function colorRefresh() {
-  // loop through each class of '.time-block'
-  $('.time-block').each(function() {
-    const blockHour = parseInt(this.id);
-    if (blockHour = currentHour) {
-      $(this).removeClass('past future').addClass('present');
-    } else if (blockHour < currentHour) {
-      $(this).removeClass('present future').addClass('past');
-    } else if (blockHour > currentHour) {
-      $(this).removeClass('past present').addClass('future');
-    }
-  });
-} 
-
-// -- Parse user data from localStorage onto page"
-// gets user input from localStorage and places it on the <textarea> values when the user
-// goes on the page next time. 
-$('.time-block').each(function(){
-  const key = $(this).attr('id');
-  const value = localStorage.getItem(key);
-  $(this).children('.description').val(value);
-
-});
-
-// -- FUNCTION 5 --
-// Using DayJS to implement up-to-date date and time
-function updateTime() {
-  const dateEl = $('#date');
-  const timeEl = $('#time');
-  const currentDate = dayjs().format('dddd, MMMMM D, YYYY');
-  const currentTime = dayjs().format('hh:mm:ss A');
+  // -- FUNCTION 4 --
+  // function to change color block based on past (grey, present (red), future (green), relative to the currentHour
+  function colorRefresh() {
+    // loop through each class of '.time-block'
+    $('.time-block').each(function() {
+      const blockHour = parseInt(this.id);
+      
+      if (blockHour === currentHour) {
+        $(this).removeClass('past future').addClass('present');
+      } else if (blockHour < currentHour) {
+        $(this).removeClass('future present').addClass('past');
+      } else {
+        $(this).removeClass('past present').addClass('future');
+      }
+    });
+  }
   
-  dateEl.text(currentDate);
-  timeEl.text(currentTime);
-}
+  
+
+  // -- Parse user data from localStorage onto page"
+  // gets user input from localStorage and places it on the <textarea> values when the user
+  // goes on the page next time. 
+  $('.time-block').each(function() {
+    const key = $(this).attr('id');
+    const value = localStorage.getItem(key);
+    $(this).children('.description').val(value);
+
+  });
+
+  // -- FUNCTION 5 --
+  // Using DayJS to implement up-to-date date and time
+  function updateTime() {
+    const dateEl = $('#date');
+    const timeEl = $('#time');
+    const currentDate = dayjs().format('dddd, MMMM D, YYYY');
+    const currentTime = dayjs().format('hh:mm:ss A');
+
+    dateEl.text(currentDate);
+    timeEl.text(currentTime);
+  }
+
+  
+
+  hourlyColor();
+  textEntry();
+  colorRefresh();
+
+  setInterval(updateTime, 1000);
 
 
 
